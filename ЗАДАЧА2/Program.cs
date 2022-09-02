e7ac907a051fc5f85dbe9a0485e53553d512fab5
Zaur199 <zaur.rojer@mail.ru>
2022-09-02 16:54:33 +0300

    TASK2

---
+++ ЗАДАЧА2/Program.cs
@@ -0,0 +1,23 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] arr = new int[123];
+int count = 0;
+int i = 0;
+int j = 0;
+while(i < arr.Length)
+{
+    arr[i] = new Random().Next(5, 660);
+    Console.Write(arr[i] + " ");
+    i++;
+}
+
+while(j < arr.Length)
+{
+    if(arr[j] > 9 && arr[j] < 100)
+    {
+        count++; 
+    }
+    j++;
+}
+
+Console.WriteLine();
+Console.WriteLine("В массиве содержится следующее количество элементов из отрезка [10,99]: " + count);
