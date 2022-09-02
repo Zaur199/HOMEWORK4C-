57d27a4023e323d7086aeba7e711a2769b2547f1
Zaur199 <zaur.rojer@mail.ru>
2022-09-02 16:52:43 +0300

    Task1

---
+++ ЗАДАЧА1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Определить, присутствует ли в заданном массиве, некоторое число
+Console.WriteLine("Введите число: a" );
+int[] array = {1, 3, 5, 9, 1, -4, -7, 56, -113};
+int n = array.Length;
+int a = int.Parse(Console.ReadLine());
+int find = a;
+int i = 0;
+while(i < n)
+{
+    if(array[i] == find)
+    {
+        Console.WriteLine("СТОП: ДАННОЕ ЧИСЛО НАШЛОСЬ В УКАЗАННОМ МАССИВЕ. РЕЗУЛЬТАТ ПРИНЯТЬ ЗА ИСТИНУ");
+        break;
+    }
+    
+    else
+        Console.WriteLine("Данного числа нет в массиве");
+        
+i++;
+
+}
+
