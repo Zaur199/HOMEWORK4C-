851ee254d0f62513ecdb2336e9cecb2f0940e16c
Zaur199 <zaur.rojer@mail.ru>
2022-09-02 16:54:03 +0300

    TASK1.1

---
+++ ЗАДАЧА1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Определить, присутствует ли в заданном массиве, некоторое число
+//ДРУГОЙ ВАРИАНТ РЕШЕНИЯ
+bool FindElement(int[] arr, int a )
+{
+    int i = 0;
+    bool result = false;
+    for(i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == a)
+        {
+            result = true;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int[] arr = {5, 9, 78, 6, -8, 0, -45, 21, -1, 4};
+if(FindElement(arr, a ) == true)
+{
+   Console.WriteLine($"Число {a} входит в массив"); 
+}
+
+else
+    Console.WriteLine($"Число {a} не входит в массив");
+
+Console.WriteLine();
